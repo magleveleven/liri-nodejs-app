5aaef37ea8427596a507d3bd373ed9aa2c285831
Eric Ballesteros <ballesteros@icloud.com>
2019-08-04 14:25:07 -0500

    completed functions

--- README.md
+++ README.md
@@ -1,18 +1,44 @@
 # liri-node-app
 
 1. Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
-    Easy to use app for finding song, concert, and movie information by havin guser answer intuitively to questions.
+    Easy to use app for finding song, concert, and movie information by having the user type a command following a term. 
 
 2. Give a high-level overview of how the app is organized   
+    We will have 3 external connections, one for each category within the app: 
+        1) Bands in Town for Concert details
+        2) OMDB for movie details 
+        3) Spotify for song details
+
+    We will create constructors for each of the 3 categories.
+
+    We will use the command to dictate which function to run 
+
+    Each function will be designed to provide the user with the appropriate details for that category. 
 
 3. Give start-to-finish instructions on how to run the app
+    SETUP 
+    1) Install axios
+    2) Create js files, main_liri.js for functions, and liri.js for initiating
+    3) Ensure all require references are made to supporting files, i.e. keys.js, axios, spotify api, fs, moment.
+    ORGANIZE
+    1) Established succesful connections to external sources, tested with constants.
+    2) Analyzed how the data requested was being returned, and how it would need to be displayed to the user.
+    3) Created unique constructors to receive categories from the user for catered responses 
+    
 
 4. Include screenshots, gifs or videos of the app functioning
 
+
 5. Contain a link to a deployed version of the app
 
+
 6. Clearly list the technologies used in the app
-    Spotify API
-    Open Movie API
+node 
+npm
+axios
+constructor 
+node-spotify-api
 
 7. State your role in the app development
+I wrote it.  
+
--- liri.js
+++ liri.js
@@ -1,169 +1,93 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
-var axios = require("axios");
-
-var fs = require('fs');
-
-var moment = require('moment'); 
-
-
-//-----------------------------------BANDS IN TOWN-----------------------------//
-
-var artist = process.argv[3];
-//var artist = data.split(' ').join('');
-
-
-axios
-  //.get("https://rest.bandsintown.com/artists/sonvolt/events?app_id=codingbootcamp")
-  .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-
-  .then(function(response) {
-
-    console.log(response.data);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
-
-
-//-----------------------------------MOVIES-----------------------------//
-
-var movieName = process.argv[2];
-
-// Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-//console.log(queryUrl);
-
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Title: " + response.data.Title + 
-    "\nRelease Year: " + response.data.Year + 
-    "\nIMDB Rating: " + response.data.imdbRating + 
-    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
-    "\nProduced in: " + response.data.Country + 
-    "\nLanguage: " + response.data.Language + 
-    "\nPlot: " + response.data.Plot + 
-    "\nActors: " + response.data.Actors);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
-
-//-----------------------------------SONG / SPOTIFY-----------------------------//
- 
-
 var Spotify = require('node-spotify-api');
-
-var spotify = new Spotify(keys.spotify);
-
-console.log(spotify);
-
-var song = process.argv[4];
-console.log(song);
-
+var axios = require("axios");
+var moment = require('moment'); 
+var fs = require ("fs");
+var category = process.argv[2];
+var term = process.argv.slice(3).join(" ");
+const chalk = require('chalk');
 
 
+switch(category) {
+    case "spotify-this":
+        song();
+        break;
+    case "movie-this":
+        movie();
+        break;
+    case "concert-this":
+        concert();
+        break;
+        };
+    
+//-----------------------------------CONCERT / BANDS IN TOWN-----------------------------//
+function concert() {
+    var concertTerm = term.split(' ').join('+');
+    var concertURL = "https://rest.bandsintown.com/artists/"+concertTerm+"/events?app_id=codingbootcamp";
+    
+    axios.get(concertURL).then(function(response) {
+            var concertResponse = response.data;
+            var concertData = [
+            'Venue: ' + concertResponse[0].venue.name,
+            'Location: ' + concertResponse[0].venue.city + ", " +concertResponse[0].venue.country,
+            'Date: ' + moment(concertResponse[0].datetime).format('L')
+        ].join('\n');
+        console.log(chalk.cyan(concertData));
+        })
+}
+//-----------------------------------SONG / SPOTIFY-----------------------------//
+    
+function song() {
+    var songTerm = term.split(' ').join('+');
+    var spotify = new Spotify(keys.spotify);    
 
-//--------------------------------------------------
+    spotify.search({ type: 'track', query: songTerm, limit: 1}).then(function (response) {
+        var tracks = response.tracks.items;
+        tracks.forEach(function (track) {
 
+        var artists = track.artists; 
+        var artistsData = [];
+            artists.forEach(function (artist) {
+            artistsData = [
+                artist.name
+              ].join(' * ');
+            });
 
+            var trackData = [
+                'Artist (s): ' + artistsData,
+                'Song: ' + track.name,
+                'Album: ' + track.album.name,
+                track.preview_url ? 'Preview URL: ' + track.preview_url : 'Preview URL: ' + 'none provided'
+              ].join('\n');
+              console.log(chalk.blue(trackData));
+            });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+  //-----------------------------------MOVIES / OMDB-----------------------------//
+function movie () {
+    if (!term) {
+      term = 'Mr. Nobody';
+    }
+    var movieName = term.split(' ').join('+');
+    var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        console.log(movieUrl);
 
-//   * Title of the movie.
-//   * Year the movie came out.
-//   * IMDB Rating of the movie.
-//   * Rotten Tomatoes Rating of the movie.
-//   * Country where the movie was produced.
-//   * Language of the movie.
-//   * Plot of the movie.
-//   * Actors in the movie.
+    axios.get(movieUrl).then(function(response) {
 
+        var movieData = [
+        'Title: ' + response.data.Title,
+        'Year: ' + response.data.Year,
+        'IMDB Rating: ' + response.data.imdbRating,
+        'Country: ' + response.data.Country,
+        'Language(s): ' + response.data.Language,
+        'Plot: ' + response.data.Plot,
+        'Actors: ' + response.data.Actors
+        ].join('\n');
+        console.log(chalk.green(movieData));
 
-
-    //Make it so liri.js can take in one of the following commands:
-        //concert-this
-
-            //node liri.js concert-this <artist/band name here>
-
-            //This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-
-                //Name of the venue
-
-                //Venue location
-
-                //Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-        //spotify-this-song
-
-            //node liri.js spotify-this-song '<song name here>'
-
-            //This will show the following information about the song in your terminal/bash window
-
-                //Artist(s)
-
-                //The song's name
-
-                //A preview link of the song from Spotify
-
-                //The album that the song is from
-
-                //If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-        //movie-this
-
-            //node liri.js movie-this '<movie name here>'
-
-            //This will output the following information to your terminal/bash window:
-
-                    //* Title of the movie.
-                    //* Year the movie came out.
-                    //* IMDB Rating of the movie.
-                    //* Rotten Tomatoes Rating of the movie.
-                    //* Country where the movie was produced.
-                    //* Language of the movie.
-                    //* Plot of the movie.
-                    //* Actors in the movie.
-                //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-        //do-what-it-says
-        
-            //node liri.js do-what-it-says
-
-                //Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-        
-            //It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-        
-            //Edit the text in random.txt to test out the feature for movie-this and concert-this.
+})
+};
